d0d5f5d4bdbca1674e8adbb8aeac92036c405d03
Caceresenzo <caceresenzo1502@gmail.com>
2024-06-30 15:40:42 +0200

    pass 3rd stage

--- src/server.cpp
+++ src/server.cpp
@@ -11,6 +11,81 @@
 
 #define PORT 4221
 
+size_t recv_line(int fd, std::string &result)
+{
+  result.clear();
+
+  while (true)
+  {
+    char value = 0;
+
+    if (recv(fd, &value, 1, 0) != 1)
+    {
+      return (std::string::npos);
+    }
+
+    if (value == '\n')
+    {
+      break;
+    }
+
+    result += value;
+  }
+
+  if (!result.empty() && result.back() == '\r')
+  {
+    result.erase(result.end() - 1);
+  }
+
+  return (result.size());
+}
+
+enum class Method
+{
+  UNKNOWN,
+  GET,
+  POST,
+};
+
+Method
+method_parse(const std::string &input)
+{
+  if ("GET" == input)
+    return (Method::GET);
+
+  if ("POST" == input)
+    return (Method::POST);
+
+  return (Method::UNKNOWN);
+}
+
+struct Request
+{
+  Method method;
+  std::string path;
+};
+
+Request request_parse(int client_fd)
+{
+  Request request;
+
+  std::string line;
+  recv_line(client_fd, line);
+
+  {
+    const std::string delimiter = " ";
+
+    size_t offset = line.find(delimiter);
+    request.method = method_parse(line.substr(0, offset));
+    line.erase(0, offset + delimiter.length());
+
+    offset = line.find(delimiter);
+    request.path = line.substr(0, offset);
+  }
+
+  return (request);
+}
+
 int main(int argc, char **argv)
 {
   std::cout << std::unitbuf;
@@ -19,14 +94,14 @@ int main(int argc, char **argv)
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (server_fd < 0)
   {
-    std::cerr << "Failed to create server socket" << std::endl;
+    perror("socket");
     return (EXIT_FAILURE);
   }
 
   int reuse = 1;
   if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0)
   {
-    std::cerr << "setsockopt failed" << std::endl;
+    perror("setsockopt");
     return (EXIT_FAILURE);
   }
 
@@ -37,14 +112,14 @@ int main(int argc, char **argv)
 
   if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) != 0)
   {
-    std::cerr << "bind failed" << std::endl;
+    perror("bind");
     return (EXIT_FAILURE);
   }
 
   int connection_backlog = 5;
   if (listen(server_fd, connection_backlog) != 0)
   {
-    std::cerr << "listen failed" << std::endl;
+    perror("listen");
     return (EXIT_FAILURE);
   }
 
@@ -58,8 +133,18 @@ int main(int argc, char **argv)
   int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_len);
   std::cout << "Client connected" << std::endl;
 
-  const char* response = "HTTP/1.1 200 OK\r\n\r\n";
-  send(client_fd, response, strlen(response), 0);
+  Request request = request_parse(client_fd);
+
+  if (request.path == "/")
+  {
+    const char *response = "HTTP/1.1 200 OK\r\n\r\n";
+    send(client_fd, response, strlen(response), 0);
+  }
+  else
+  {
+    const char *response = "HTTP/1.1 404 OK\r\n\r\n";
+    send(client_fd, response, strlen(response), 0);
+  }
 
   close(server_fd);
   close(client_fd);
